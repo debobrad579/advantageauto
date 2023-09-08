fec08361bf6247c55b12910232a74d39301eb582
Brady <thebradster7.bd@gmail.com>
2023-09-08 16:08:53 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import jsconfigPaths from "vite-jsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/advantageauto",
+  base: "./advantageauto",
   plugins: [react(), jsconfigPaths()],
 })
