0142813bee1107e1892c7fdb8829ed5bf1e07962
Brady <thebradster7.bd@gmail.com>
2023-09-26 11:00:13 -0400

    removed commented code

--- src/pages/Appointment/Modal.jsx
+++ src/pages/Appointment/Modal.jsx
@@ -52,37 +52,3 @@ export function Modal({ error }) {
     document.getElementById("modal-container")
   )
 }
-
-// export function Modal({ error }) {
-//   const modalRef = useRef(null)
-
-//   useEffect(() => {
-//     if (error?.success != null && process.env.NODE_ENV !== "test") {
-//       modalRef.current.showModal()
-//     }
-//   }, [error])
-
-//   function handleClick(e) {
-//     const modalDimentions = modalRef.current.getBoundingClientRect()
-
-//     if (
-//       e.clientX < modalDimentions.left ||
-//       e.clientX > modalDimentions.right ||
-//       e.clientY < modalDimentions.top ||
-//       e.clientY > modalDimentions.bottom
-//     )
-//       modalRef.current.close()
-//   }
-
-//   return createPortal(
-//     <dialog className="modal" ref={modalRef} onClick={handleClick}>
-//       <p>
-//         {error?.success
-//           ? "Request Submitted Successfully"
-//           : "Request Submission Failed"}
-//       </p>
-//       <button onClick={() => modalRef.current.close()}>Close</button>
-//     </dialog>,
-//     document.getElementById("modal-container")
-//   )
-// }
